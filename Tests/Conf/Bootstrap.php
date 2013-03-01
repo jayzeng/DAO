1da85e6dcbdf6add4ad86ccfd4832aed5699ea2b
Jay Zeng <jayzeng@jay-zeng.com>
2013-02-28 23:45:32 -0800

    adjust path

--- Tests/Conf/Bootstrap.php
+++ Tests/Conf/Bootstrap.php
@@ -6,5 +6,5 @@ ini_set('display_errors','On');
 ini_set('display_startup_errors','On');
 
 // Autload project level bootstrap file
-require_once( __DIR__ . DIRECTORY_SEPARATOR . '../Vendors/autoload.php' );
+require_once( __DIR__ . DIRECTORY_SEPARATOR . '../../Vendors/autoload.php' );
 ?>
